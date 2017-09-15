4efd59872913b1ee4580658eef2e40db0993bca7
Stuart Axon <stuaxo2@yahoo.com>
2017-09-15 01:05:55 +0100

    Add skeleton flask app

---
+++ homework/tests/tests.py
@@ -0,0 +1,19 @@
+from app import app
+import unittest
+
+class FlaskBasicTests(unittest.TestCase):
+
+    def setUp(self):
+        self.app = app.test_client()
+        self.app.testing = True
+
+    def tearDown(self):
+        pass
+
+    def test_for_index_response(self):
+        result = self.app.get('/')
+        self.assertEqual(result.status_code, 200)
+
+    def test_for_hello_world(self):
+        result = self.app.get('/')
+        self.assertEqual(result.data, "Hello World!")
